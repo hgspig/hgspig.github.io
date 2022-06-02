6c8072fc5c7609242d2c720de852108a397b3227
Hannah <troll@monstercode.com>
2022-06-02 19:27:29 -0400

    changed name response message

--- scripts/main.js
+++ scripts/main.js
@@ -18,7 +18,7 @@ function setUserName() {
       setUserName();
     } else {
       localStorage.setItem('name', myName);
-      myHeading.textContent = 'Books are terrific, ' + myName;
+      myHeading.textContent = 'Books are awesome, ' + myName;
     }
   }
 if(!localStorage.getItem('name')) {
