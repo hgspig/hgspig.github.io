41a4d2a7a6c749226448d910e99ab77516c72cb0
Hannah <troll@monstercode.com>
2022-06-02 19:20:38 -0400

    Initial commit

---
+++ scripts/main.js
@@ -0,0 +1,33 @@
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/books-test-site.jpg') {
+      myImage.setAttribute('src','images/B&WBooks.jpg');
+    } else {
+      myImage.setAttribute('src','images/books-test-site.jpg');
+    }
+}
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+    let myName = prompt('Please enter your name.');
+    if(!myName) {
+      setUserName();
+    } else {
+      localStorage.setItem('name', myName);
+      myHeading.textContent = 'Books are terrific, ' + myName;
+    }
+  }
+if(!localStorage.getItem('name')) {
+    setUserName();
+} else {
+    let storedName = localStorage.getItem('name');
+    myHeading.textContent = 'Books are terrific, ' + storedName;
+}
+
+myButton.onclick = function() {
+    setUserName();
+  }
